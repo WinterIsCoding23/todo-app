9918fb4c762e1253cf05e98d0ca55e5094e4045a
Markus Winter <markusdominicwinter@hotmail.com>
2023-09-03 20:01:30 +0200

    style labels

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,18 @@ function App() {
         <h1 className="title">The Ultimate ToDo-List</h1>
       </div>
       <div className="inputMask">
-        <label htmlFor="task">Task</label>
+        <label htmlFor="task" className="my-2 font-bold">
+          Task
+        </label>
         <input
           type="text"
           id="task"
           className="rounded  w-auto min-w-[300px]"
           placeholder="Write your task here..."
         />
-        <label htmlFor="deadline">Deadline</label>
+        <label htmlFor="deadline" className="my-2 font-bold">
+          Deadline
+        </label>
         <input
           type="text"
           id="deadline"
