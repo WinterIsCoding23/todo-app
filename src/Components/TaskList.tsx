1182171e43fe1c93811ac7c5c026ad019bab0423
Markus Winter <markusdominicwinter@hotmail.com>
2023-09-07 17:18:03 +0200

    map over todo to render each task in a separate div

--- src/Components/TaskList.tsx
+++ src/Components/TaskList.tsx
@@ -8,10 +8,12 @@ const TaskList = ({ todo }: Props) => {
     <div className="taskContainer">
       <h3>Here is the list of open ToDo-tasks:</h3>
       {todo.length > 0 ? (
-        <div>
-          <p>{todo[0].newTask}</p>
-          <p>{todo[0].newDeadline}</p>
-        </div>
+        todo.map((task, index) => (
+          <div key={index}>
+            <p>{task.newTask}</p>
+            <p>{task.newDeadline}</p>
+          </div>
+        ))
       ) : (
         <p>"You're lucky, looks like you don't have anything to do."</p>
       )}
