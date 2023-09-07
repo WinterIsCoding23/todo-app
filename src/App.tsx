09d4df6e272c26f279090e7740dd95128cb5bff8
Markus Winter <markusdominicwinter@hotmail.com>
2023-09-07 13:27:37 +0200

    declare useState-variables taskInput and deadlineInput & re-write handleSubmit with them & add value and onChange for both input-fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
 import "./index.css";
 import "./App.css";
 import TaskList from "./Components/TaskList";
 
 function App() {
+  const [taskInput, setTaskInput] = useState("");
+  const [deadlineInput, setDeadlineInput] = useState("");
+
   function handleSubmit(e: FormEvent<HTMLFormElement>): void {
     e.preventDefault();
 
     const form = e.currentTarget;
     const formData = new FormData(form);
 
-    const taskInput = form.querySelector(
-      'input[name="task"]'
-    ) as HTMLInputElement;
-    const deadlineInput = form.querySelector(
-      'input[name="deadline"]'
-    ) as HTMLInputElement;
+    const taskValue = formData.get("task") as string;
+    const deadlineValue = formData.get("deadline") as string;
+    console.log(taskValue, deadlineValue);
 
-    const formJson: Record<string, FormDataEntryValue> = Object.fromEntries(
-      formData.entries()
-    );
-    console.log(formJson.task, formJson.deadline);
+    // const formJson: Record<string, FormDataEntryValue> = Object.fromEntries(
+    //   formData.entries()
+    // );
+    // console.log(formJson.task, formJson.deadline);
 
-    if (taskInput && deadlineInput) {
-      taskInput.value = "";
-      deadlineInput.value = "";
-    }
+    setTaskInput("");
+    setDeadlineInput("");
   }
 
   return (
@@ -41,6 +39,8 @@ function App() {
           type="text"
           id="task"
           name="task"
+          value={taskInput}
+          onChange={(e) => setTaskInput(e.target.value)}
           className="rounded  w-auto min-w-[300px]"
           placeholder="Write your task here..."
         />
@@ -51,6 +51,8 @@ function App() {
           type="text"
           id="deadline"
           name="deadline"
+          value={deadlineInput}
+          onChange={(e) => setDeadlineInput(e.target.value)}
           className="rounded w-auto min-w-[300px]"
           placeholder="When should this task be completed?"
         />
