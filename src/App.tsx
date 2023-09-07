6244a6cb7cbb63cb52f69d1c532969235b0a45ee
Markus Winter <markusdominicwinter@hotmail.com>
2023-09-07 12:42:17 +0200

    add name-props to input-fields & create handleSubmit-function

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { FormEvent } from "react";
 import "./index.css";
 import "./App.css";
 import TaskList from "./Components/TaskList";
 
 function App() {
+  function handleSubmit(e: FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+
+    const formJson: Record<string, FormDataEntryValue> = Object.fromEntries(
+      formData.entries()
+    );
+    console.log(formJson);
+  }
+
   return (
     <div className="App">
       <div className="header">
         <h1 className="title">The Ultimate ToDo-List</h1>
       </div>
-      <form className="inputMask">
+      <form className="inputMask" onSubmit={handleSubmit}>
         <label htmlFor="task" className="my-2 font-bold">
           Task
         </label>
         <input
           type="text"
           id="task"
+          name="task"
           className="rounded  w-auto min-w-[300px]"
           placeholder="Write your task here..."
         />
@@ -25,10 +38,11 @@ function App() {
         <input
           type="text"
           id="deadline"
+          name="deadline"
           className="rounded w-auto min-w-[300px]"
           placeholder="When should this task be completed?"
         />
-        <button className="inputButton" onClick={}>
+        <button type="submit" className="inputButton">
           OK
         </button>
       </form>
